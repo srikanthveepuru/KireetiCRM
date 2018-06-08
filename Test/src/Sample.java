596b1135059bbeb3240a9a7760b27164373dd079
HP <HP@HP-PC>
2018-06-08 20:48:38 +0530

    Commited by Srikanth

---
+++ Test/src/Sample.java
@@ -0,0 +1,14 @@
+public class Sample{
+	
+	public static void main(String[] args) {
+		//System.out.println("ldsjfglkjsdflkgjdfs");
+		int a=addition(5,5);
+		System.out.println("Output : "+a);
+	}
+	
+	public static int addition(int x,int y) {
+		int z=x+y;
+		
+		return z;
+	}
+}
