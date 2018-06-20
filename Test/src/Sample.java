2392c5dc63cc74a47d39dd0e2e21544cf1bbc2ba
srikanthveepuru <v.srikanth@kireetisoft.com>
2018-06-21 04:30:06 +0530

    Removed one comment in main method

--- Test/src/Sample.java
+++ Test/src/Sample.java
@@ -5,7 +5,7 @@ public class Sample{
 	}
 	
 	public static void main(String[] args) {
-		//System.out.println("ldsjfglkjsdflkgjdfs");
+		
 		int a=addition(5,5);
 		System.out.println("Output : "+a);
 	}
